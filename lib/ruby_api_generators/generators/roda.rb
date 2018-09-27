b65abc380b4171127dd44374fbd831d6eeddc9a2
Vitor Oliveira <vbrazo@gmail.com>
2018-09-27 14:30:20 -0300

    Add set_up_application_folder and build_test_suite_folder thor methods

--- lib/ruby_api_generators/generators/roda.rb
+++ lib/ruby_api_generators/generators/roda.rb
@@ -28,6 +28,53 @@ module RubyApiGenerators
       def copy_config_ru
         copy_file 'config.ru', "#{name}/config.ru"
       end
+
+      def copy_rakefile
+        copy_file 'Rakefile', "#{name}/Rakefile"
+      end
+
+      def set_up_application_folder
+        build_application_folder        
+        build_models_folder
+        build_routes_folder
+        build_config_folder
+        build_db_folder
+      end
+
+      def build_test_suite_folder
+        empty_directory "#{name}/spec"
+        copy_file 'spec/rspec.options', "#{name}/.rspec"
+        template 'spec_helper.rb.erb', "#{name}/spec_helper.rb"
+      end
+
+      private
+
+      def build_application_folder
+        empty_directory "#{name}/application"       
+        copy_file "application/api.rb", "#{name}/application/api.rb"
+        copy_file "application/api/api_support.rb", "#{name}/application/api/api_support.rb"
+      end
+
+      def build_models_folder
+        empty_directory "#{name}/application/models"
+        copy_file "application/api/models/model1.rb", "#{name}/application/models/model1.rb"
+      end
+
+      def build_routes_folder
+        empty_directory "#{name}/application/routes"
+        copy_file "application/api/routes/main.rb", "#{name}/application/api/routes/main.rb"
+      end
+
+      def build_config_folder
+        empty_directory "#{name}/application/config"
+        copy_file "application/config/database.rb", "#{name}/application/config/database.rb"
+        copy_file "application/config/sequel.rb", "#{name}/application/config/sequel.rb"
+      end
+
+      def build_db_folder
+        empty_directory "#{name}/application/db"
+        copy_file "application/db/migrations/1_user.rb", "#{name}/application/db/migrations/1_user.rb"
+      end
     end
   end
 end
