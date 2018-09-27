53a5887fe5ff6fe59ffa6b6a86bac0d05da57c33
Vitor Oliveira <vbrazo@gmail.com>
2018-09-27 09:13:45 -0300

    Minor changes

--- lib/ruby_api_generators/cli.rb
+++ lib/ruby_api_generators/cli.rb
@@ -5,8 +5,8 @@ require 'thor'
 # Responsibility: handle the command line interface
 module ApiGenerators
   class CLI < Thor
-    desc 'api_generators roda', 'Generate generic Roda API via CLI'
 
+    desc 'api_generators roda', 'Generate generic Roda API via CLI'
     # CLI method to generate generic roda API
     def roda
       # TODO
@@ -15,6 +15,7 @@ module ApiGenerators
       exit 1
     end
 
+    desc 'api_generators sinatra', 'Generate generic Sinatra API via CLI'
     # CLI method to generate generic sinatra API
     def sinatra
       # TODO
