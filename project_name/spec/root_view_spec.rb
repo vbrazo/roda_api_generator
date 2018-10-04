24496b6b787f9930fa51c30d8fc43d2e3000b1e2
Vitor Oliveira <vbrazo@gmail.com>
2018-10-04 16:32:37 -0300

    Add roda-http-auth and update Rakefile

---
+++ project_name/spec/root_view_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe 'Do a GET request in the root_path' do
+  it 'should render root page' do
+    get '/'
+
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to eq('Nothing Here')
+  end
+
+  it 'should render example page' do
+    get '/example', email: 'email'
+
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to eq('email')
+  end
+end
