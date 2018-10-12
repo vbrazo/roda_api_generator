3235ca68581833ead41b8d275252e9c35b50d097
Vitor Oliveira <vbrazo@gmail.com>
2018-10-12 19:59:40 -0300

    Remove /example/.bundle

--- example/.bundle/gems/roda-http-auth-0.2.0/lib/roda/plugins/http_auth/version.rb
+++
@@ -1,7 +0,0 @@
-class Roda
-  module RodaPlugins
-    module HttpAuth
-      VERSION = "0.2.0"
-    end
-  end
-end
--- example/.bundle/gems/rspec_sequel_matchers-0.4.0/lib/rspec_sequel_matchers.rb
+++
@@ -1,9 +0,0 @@
-# Load base
-["base", "association", "validation"].each do |file|
-  require File.join(File.dirname(__FILE__), "rspec_sequel", file)
-end
-
-# Add matchers
-Dir[File.join(File.dirname(__FILE__), "rspec_sequel", "matchers", "*.rb")].each do |file|
-  require file
-end
